aa7d88e9aaa60140a841ad52c5773cae6dfbebad
evanc-mfw <cowin.evan@moneyforward.co.jp>
2024-11-14 18:02:24 +0900

    change workflow to publish plugin to gradle portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,10 @@ plugins {
     kotlin("jvm") version "2.0.20"
     id("com.gradle.plugin-publish") version "1.2.1"
     `java-gradle-plugin`
-    `maven-publish`
 }
 
 group = "com.moneyforward.gradle"
-version = "0.0.0"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -16,30 +15,15 @@ tasks.test {
     useJUnitPlatform()
 }
 
-dependencies {
-    implementation("org.jline:jline-reader:3.27.1")
-}
-
 gradlePlugin {
+    website = "https://github.com/moneyforward/gradle-private-repository-plugin"
+    vcsUrl = "https://github.com/moneyforward/gradle-private-repository-plugin"
     plugins {
         create("privateRepositoryPlugin") {
             displayName = "Private Repository Plugin"
-            description = "This plugin eases the "
+            description = "This plugin reduces repetition when specifying private GitHub packages as gradle dependencies"
             id = "com.moneyforward.private-repository-plugin"
             implementationClass = "com.moneyforward.gradle.PrivateRepositoryPlugin"
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/moneyforward/gradle-private-repository-plugin")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
         }
     }
 }
